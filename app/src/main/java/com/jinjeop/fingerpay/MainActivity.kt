30df610774c9363b720d5a13939b29bcf935a4a2
kmc7582s <kmc7582s@gmail.com>
2023-07-19 23:34:00 +0900

    2023 7/19

--- app/src/main/java/com/jinjeop/fingerpay/MainActivity.kt
+++ app/src/main/java/com/jinjeop/fingerpay/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.jinjeop.fingerpay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logo = findViewById<ImageView>(R.id.logo_img)
+
+        logo.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
 
 }
